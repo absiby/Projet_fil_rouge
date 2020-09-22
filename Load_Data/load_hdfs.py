dd0eac3b3011f8a95295d48d0bf088b30ef004ac
samuel <samuel.beau@gmail.com>
2020-09-22 21:25:19 +0200

    commit 22-09 n1: samuel

---
+++ Load_Data/load_hdfs.py
@@ -0,0 +1,9 @@
+#########################Exporter le fichier de données json de enedis sur hdfs.#################################""
+from hdfs import InsecureClient
+client = InsecureClient('http://localhost:50070', user='cloudera')
+client.makedirs('data')
+print(client.list('/user/cloudera'))
+
+client.write('/home/fitec/projet_fil_rouge/source_des_données/data/consommation_elec.json', '/user/cloudera/data')
+
+
