5ff2b32f8593a67b921b84c7c532fc1ba93e2856
samuel <samuel.beau@gmail.com>
2020-09-26 22:44:35 +0200

    commit n6 26_09 - branch_samuel

--- Load_Data/load_hdfs.py
+++ Load_Data/load_hdfs.py
@@ -7,6 +7,6 @@ client = InsecureClient('http://localhost:50070', user='cloudera')
 client.makedirs('data')
 print(client.list('/user/cloudera'))
 
-#client.write('/home/fitec/projet_fil_rouge/source_des_données/data/consommation_elec.json', '/user/cloudera/data')
-client.upload('/user/cloudera/data', '/home/fitec/projet_fil_rouge/source_des_données/data/consommation_elec.json')
+client.write('/home/fitec/projet_fil_rouge/source_des_données/data/consommation_elec.json', '/user/cloudera/data', overwrite=True)
+#client.upload('/user/cloudera/data', '/home/fitec/projet_fil_rouge/source_des_données/data/consommation_elec.json')
 
--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import os
 
-#print("1 - Démarrage de Cassendra")
-#os.system("sudo docker run --name fitec-cassandra -v /home/fitec/FITEC/Cassandra:/var/lib/cassandra -p 9042:9042 -d --rm cassandra")
-#os.system("echo Fin Démarrage de Cassendra")
-
-#print("2 - Démarrage de HDFS")
-#os.system("sudo docker run --hostname=quickstart.cloudera --privileged=true -t -i -v /home/fitec/FITEC/hadoop/cloudera:/src --publish-all=true -p 8888:8888 -p 90:90 -p 7180:7180 -p 50070:50070 -p 10002:10002 cloudera/quickstart /usr/bin/docker-quickstart")
-#os.system("sudo docker run --hostname=quickstart.cloudera --privileged=true -t -d --rm -v /home/fitec/FITEC/hadoop/cloudera:/src --publish-all=true -p 8888:8888 -p 90:90 -p 7180:7180 -p 50070:50070 -p 10002:10002 cloudera/quickstart /usr/bin/docker-quickstart")
-#os.system("echo Fin Démarrage de HDFS")
-
 print("3 - Get Data")
 os.system("python3 /home/fitec/FITEC/Projet_fil_rouge/Get_Data/get_data_enedis.py")
 os.system("python3 /home/fitec/FITEC/Projet_fil_rouge/Get_Data/get_data_meteofrance.py")
