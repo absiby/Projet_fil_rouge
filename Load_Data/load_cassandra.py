d95ac619981c36730ef1aa50df84f72172c531c3
samuel <samuel.beau@gmail.com>
2020-09-27 16:35:59 +0200

    commit n1 21_09

--- Load_Data/load_cassandra.py
+++ Load_Data/load_cassandra.py
@@ -9,14 +9,14 @@ def load_meteofrance():
 
     session.execute("CREATE KEYSPACE IF NOT EXISTS meteofrancedb WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor': 1}")
     session.execute('use meteofrancedb')
-    session.execute('CREATE TABLE IF NOT EXISTS meteofrance(numer_sta INT primary key, date VARCHAR, pmer VARCHAR, tend VARCHAR, cod_tend VARCHAR)')
+    session.execute('CREATE TABLE IF NOT EXISTS meteofrance(numer_sta INT primary key,date VARCHAR,pmer VARCHAR,tend VARCHAR,cod_tend VARCHAR,dd VARCHAR,ff VARCHAR,t VARCHAR,td VARCHAR,u VARCHAR,vv VARCHAR,ww VARCHAR,w1 VARCHAR,w2 VARCHAR,n VARCHAR,nbas VARCHAR,hbas VARCHAR,cl VARCHAR,cm VARCHAR,ch VARCHAR,pres VARCHAR,niv_bar VARCHAR,geop VARCHAR,tend24 VARCHAR,tnN VARCHAR,txN VARCHAR,tminsol VARCHAR,sw VARCHAR,tw VARCHAR,raf10 VARCHAR,rafper VARCHAR,per VARCHAR,etat_sol VARCHAR,ht_neige VARCHAR,ssfrai VARCHAR,perssfrai VARCHAR,rrN VARCHAR,phenspeN VARCHAR,nnuageN VARCHAR,ctypeN VARCHAR,hnuageN VARCHAR)')
 
     prepared = session.prepare("""
-            INSERT INTO meteofrance (numer_sta, date, pmer, tend, cod_tend)
-            VALUES (?, ?, ?, ?, ?)
+            INSERT INTO meteofrance (numer_sta,date,pmer,tend,cod_tend,dd,ff,t,td,u,vv,ww,w1,w2,n,nbas,hbas,cl,cm,ch,pres,niv_bar,geop,tend24,tnN,txN,tminsol,sw,tw,raf10,rafper,per,etat_sol,ht_neige,ssfrai,perssfrai,rrN,phenspeN,nnuageN,ctypeN,hnuageN)
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
             """)
 
-    with open("/home/fitec/projet_fil_rouge/source_des_données/data/meteofrance.csv", "r") as rows:
+    with open("/home/fitec/projet_fil_rouge/source_des_données/data/donnees-synop-essentielles-omm.csv", "r") as rows:
         i = 0    
         for row in rows:
             if i == 1:
@@ -26,12 +26,49 @@ def load_meteofrance():
                 pmer = columns[2]
                 tend = columns[3]
                 cod_tend = columns[4]
+                dd = columns[5]
+                ff = columns[6]
+                t = columns[7]
+                td = columns[8]
+                u = columns[9]
+                vv = columns[10]
+                ww = columns[11]
+                w1 = columns[12]
+                w2 = columns[13]
+                n = columns[14]
+                nbas = columns[15]
+                hbas = columns[16]
+                cl = columns[17]
+                cm = columns[18]
+                ch = columns[19]
+                pres = columns[20]
+                niv_bar = columns[21]
+                geop = columns[22]
+                tend24 = columns[23]
+                tnN = columns[24]
+                txN = columns[25]
+                tminsol = columns[26]
+                sw = columns[27]
+                tw = columns[28]
+                raf10 = columns[29]
+                rafper = columns[30]
+                per = columns[31]
+                etat_sol = columns[32]
+                ht_neige = columns[33]
+                ssfrai = columns[34]
+                perssfrai = columns[35]
+                rrN = columns[36]
+                phenspeN = columns[37]
+                nnuageN = columns[38]
+                ctypeN = columns[39]
+                hnuageN = columns[40]
 
-                session.execute(prepared, [numer_sta, date, pmer, tend, cod_tend])
+
+                session.execute(prepared, [numer_sta,date,pmer,tend,cod_tend,dd,ff,t,td,u,vv,ww,w1,w2,n,nbas,hbas,cl,cm,ch,pres,niv_bar,geop,tend24,tnN,txN,tminsol,sw,tw,raf10,rafper,per,etat_sol,ht_neige,ssfrai,perssfrai,rrN,phenspeN,nnuageN,ctypeN,hnuageN]
+)
             i = 1    
 
 
 
     #closing Cassandra connection
     session.shutdown()
-
