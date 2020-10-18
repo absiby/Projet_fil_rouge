d625b5e7a8ee2fe5864a47a6d2f5724a5578f95e
samuel <samuel.beau@gmail.com>
2020-10-18 10:52:34 +0200

    commit n1 18_10

--- Shell/shell.py
+++ Shell/shell.py
@@ -7,12 +7,9 @@ os.system("chmod 777 /home/fitec/projet_fil_rouge/source_des_données/data")
 
 print("1 - Démarrage de Cassendra")
 os.system("sudo docker run --name fitec-cassandra -v /home/fitec/FITEC/Cassandra:/var/lib/cassandra -p 9042:9042 -d --rm cassandra")
-os.system("echo Fin Démarrage de Cassendra")
+print("2 - Fin Démarrage de Cassendra")
 
-print("2 - Démarrage de HDFS")
-os.system("sudo docker run --hostname=quickstart.cloudera --privileged=true -t -i --rm -v /home/fitec/FITEC/hadoop/cloudera:/src --publish-all=true -p 8888:8888 -p 90:90 -p 7180:7180 -p 50070:50070 -p 10002:10002 cloudera/quickstart /usr/bin/docker-quickstart")
-#os.system("sudo docker run --hostname=quickstart.cloudera --privileged=true -t -d --rm -v /home/fitec/FITEC/hadoop/cloudera:/src --publish-all=true -p 8888:8888 -p 90:90 -p 7180:7180 -p 50070:50070 -p 10002:10002 cloudera/quickstart /usr/bin/docker-quickstart")
-os.system("echo Fin Démarrage de HDFS")
+
 
 
 
